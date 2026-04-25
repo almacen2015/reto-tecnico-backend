6e0fa97fac3ab9bcdaff1b3e8e2982421e871632
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-25 11:05:11 -0500

    se agrego metodo en controller para crear client

--- ms-client/src/main/java/backend/msclient/presentation/controller/ClientController.java
+++ ms-client/src/main/java/backend/msclient/presentation/controller/ClientController.java
@@ -1,12 +1,14 @@
 package backend.msclient.presentation.controller;
 
+import backend.msclient.application.mapper.ClientMapper;
+import backend.msclient.application.usecase.CreateClientUseCase;
 import backend.msclient.application.usecase.GetClientByIdUseCase;
 import backend.msclient.domain.model.Client;
+import backend.msclient.presentation.dto.ClientRequest;
+import backend.msclient.presentation.dto.ClientResponse;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -16,14 +18,32 @@ import reactor.core.scheduler.Schedulers;
 public class ClientController {
 
     private final GetClientByIdUseCase getClientByIdUseCase;
+    private final CreateClientUseCase createClientUseCase;
 
-    public ClientController(GetClientByIdUseCase getClientByIdUseCase) {
+    public ClientController(GetClientByIdUseCase getClientByIdUseCase, CreateClientUseCase createClientUseCase) {
         this.getClientByIdUseCase = getClientByIdUseCase;
+        this.createClientUseCase = createClientUseCase;
     }
 
+    @PostMapping
+    public Mono<ClientResponse> createClient(@RequestBody @Valid ClientRequest request) {
+
+        log.info("Request received to create client");
+
+        return Mono.fromCallable(() -> ClientMapper.toDomain(request))
+                .map(createClientUseCase::execute)
+                .map(ClientMapper::toResponse)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnSuccess(response ->
+                        log.info("Client created with id: {}", response.id())
+                )
+                .doOnError(error ->
+                        log.error("Error creating client", error)
+                );
+    }
 
     @GetMapping("/{id}")
-    public Mono<Client> getClienteById(@PathVariable Long id) {
+    public Mono<Client> getClientById(@PathVariable Long id) {
         log.info("Request received for get client by id {}", id);
         return Mono.fromCallable(() -> getClientByIdUseCase.execute(id))
                 .subscribeOn(Schedulers.boundedElastic())
