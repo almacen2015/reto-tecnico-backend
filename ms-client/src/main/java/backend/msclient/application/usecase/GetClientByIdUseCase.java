5fdde9519cead8ae1c3bd3d0e8568de25f626218
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-25 09:54:07 -0500

    se agrego logs para GetClientByIdUseCase

--- ms-client/src/main/java/backend/msclient/application/usecase/GetClientByIdUseCase.java
+++ ms-client/src/main/java/backend/msclient/application/usecase/GetClientByIdUseCase.java
@@ -3,7 +3,11 @@ package backend.msclient.application.usecase;
 import backend.msclient.domain.exception.ClientNotFoundException;
 import backend.msclient.domain.model.Client;
 import backend.msclient.domain.repository.ClientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class GetClientByIdUseCase {
     private final ClientRepository clientRepository;
 
@@ -12,7 +16,11 @@ public class GetClientByIdUseCase {
     }
 
     public Client execute(Long id) {
+        log.info("Executing GetClientByIdUseCase with id {}", id);
         return clientRepository.findById(id)
-                .orElseThrow(() -> new ClientNotFoundException(id));
+                .orElseThrow(() -> {
+                    log.error("Client not found with id: {}", id);
+                    return new ClientNotFoundException(id);
+                });
     }
 }
--- ms-client/src/main/java/backend/msclient/presentation/controller/ClientController.java
+++ ms-client/src/main/java/backend/msclient/presentation/controller/ClientController.java
@@ -2,6 +2,7 @@ package backend.msclient.presentation.controller;
 
 import backend.msclient.application.usecase.GetClientByIdUseCase;
 import backend.msclient.domain.model.Client;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 @RestController
 @RequestMapping(name = "/api/clients")
 public class ClientController {
@@ -22,7 +24,12 @@ public class ClientController {
 
     @GetMapping("/{id}")
     public Mono<Client> getClienteById(@PathVariable Long id) {
+        log.info("Request received for get client by id {}", id);
         return Mono.fromCallable(() -> getClientByIdUseCase.execute(id))
-                .subscribeOn(Schedulers.boundedElastic());
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnSuccess(client ->
+                        log.info("Client found with id {}", client.getId()))
+                .doOnError(error ->
+                        log.error("Error getting client with id {}", id, error));
     }
 }
