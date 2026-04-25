3858d1680bc1a9cbb8d923b53a5ad50035b5a7cc
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-25 11:23:18 -0500

    se agrego clase DeleteClientUseCase

---
+++ ms-client/src/main/java/backend/msclient/application/usecase/DeleteClientUseCase.java
@@ -0,0 +1,35 @@
+package backend.msclient.application.usecase;
+
+import backend.msclient.domain.exception.ClientNotFoundException;
+import backend.msclient.domain.model.Client;
+import backend.msclient.domain.repository.ClientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DeleteClientUseCase {
+    private final ClientRepository clientRepository;
+
+    public DeleteClientUseCase(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client execute(Long id) {
+        log.info("Deleting client with identification: {}", id);
+
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Cliente not found with id: {}", id);
+                    return new ClientNotFoundException(id);
+                });
+
+        client.setStatus(false);
+
+        Client updated = clientRepository.save(client);
+
+        log.info("Cliente soft deleted with id: {}", id);
+
+        return updated;
+    }
+}
