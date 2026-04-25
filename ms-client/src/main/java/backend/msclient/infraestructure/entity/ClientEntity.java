5389841d631d53804b7e5bbe9e763c250d5b73e1
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-25 08:44:33 -0500

    se creo ClientEntity

---
+++ ms-client/src/main/java/backend/msclient/infraestructure/entity/ClientEntity.java
@@ -0,0 +1,26 @@
+package backend.msclient.infraestructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String gender;
+    private String identification;
+    private String address;
+    private String phone;
+
+    private String password;
+    private Boolean status;
+}
