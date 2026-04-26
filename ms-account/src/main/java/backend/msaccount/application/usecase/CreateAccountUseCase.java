7eeccbd4fe5cc99c3a0f8e93d9d3a8a868d838e9
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-26 10:50:55 -0500

    se agrego servicio para clientservice y se modifico CreateAccountUseCase

--- ms-account/src/main/java/backend/msaccount/application/usecase/CreateAccountUseCase.java
+++ ms-account/src/main/java/backend/msaccount/application/usecase/CreateAccountUseCase.java
@@ -2,17 +2,27 @@ package backend.msaccount.application.usecase;
 
 import backend.msaccount.domain.model.Account;
 import backend.msaccount.domain.repository.AccountRepository;
+import backend.msaccount.infraestructure.client.ClientService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateAccountUseCase {
     private final AccountRepository accountRepository;
+    private final ClientService clientService;
 
-    public CreateAccountUseCase(AccountRepository accountRepository) {
+    public CreateAccountUseCase(AccountRepository accountRepository, ClientService clientService) {
         this.accountRepository = accountRepository;
+        this.clientService = clientService;
     }
 
     public Account execute(Account account) {
+        Boolean exists = clientService.existsById(account.getClientId())
+                .block();
+
+        if (Boolean.FALSE.equals(exists)) {
+            throw new RuntimeException("Client not found");
+        }
+
         return accountRepository.save(account);
     }
 }
---
+++ ms-account/src/main/java/backend/msaccount/infraestructure/client/ClientService.java
@@ -0,0 +1,25 @@
+package backend.msaccount.infraestructure.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ClientService {
+
+    private final WebClient webClient;
+
+    public ClientService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<Boolean> existsById(Long id) {
+        return webClient.get()
+                .uri("/clients/{id}", id)
+                .retrieve()
+                .bodyToMono(Object.class)
+                .map(response -> true)
+                .onErrorResume(ex -> Mono.just(false));
+
+    }
+}
