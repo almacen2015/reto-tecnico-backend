bd2338e5b6d2a3bfee9420ccfe73eef0c280b1ab
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-26 14:40:46 -0500

    se crearon excepciones de domain

---
+++ ms-account/src/main/java/backend/msaccount/domain/exception/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package backend.msaccount.domain.exception;
+
+public class ClientNotFoundException extends RuntimeException {
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
