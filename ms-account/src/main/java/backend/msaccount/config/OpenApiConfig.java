6a01727672671993b84f2abd03a35535f19bfd60
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-26 19:44:05 -0500

    se agrego config para open en ms-account

---
+++ ms-account/src/main/java/backend/msaccount/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package backend.msaccount.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Account y Movement API")
+                        .version("1.0")
+                        .description("Microservice for managing Accounts"));
+    }
+}
