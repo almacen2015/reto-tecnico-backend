4542fd9b63685072d3fd84336bd9dcb70b22e12c
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-26 20:17:03 -0500

    se agrego metodo en GenerateReportUseCase

--- ms-account/src/main/java/backend/msaccount/application/usecase/GenerateReportUseCase.java
+++ ms-account/src/main/java/backend/msaccount/application/usecase/GenerateReportUseCase.java
@@ -1,10 +1,16 @@
 package backend.msaccount.application.usecase;
 
+import backend.msaccount.domain.model.Account;
+import backend.msaccount.domain.model.Movement;
 import backend.msaccount.domain.repository.AccountRepository;
 import backend.msaccount.domain.repository.MovementRepository;
+import backend.msaccount.presentation.dto.AccountReport;
+import backend.msaccount.presentation.dto.MovementReport;
+import backend.msaccount.presentation.dto.ReportResponse;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class GenerateReportUseCase {
@@ -17,5 +23,36 @@ public class GenerateReportUseCase {
     }
 
     public ReportResponse execute(Long clientId, LocalDate start, LocalDate end) {
+        List<Account> accounts = accountRepository.findByClientId(clientId);
+
+        List<AccountReport> accountReports = accounts.stream()
+                .map(account -> {
+
+                    List<Movement> movements =
+                            movementRepository.findByAccountIdAndDateBetween(
+                                    account.getId(),
+                                    start.atStartOfDay(),
+                                    end.atTime(23,59,59)
+                            );
+
+                    List<MovementReport> movementReports = movements.stream()
+                            .map(m -> new MovementReport(
+                                    m.getDate(),
+                                    m.getType(),
+                                    m.getAmount(),
+                                    m.getBalance()
+                            ))
+                            .toList();
+
+                    return new AccountReport(
+                            account.getNumber(),
+                            account.getType(),
+                            account.getInitialBalance(),
+                            movementReports
+                    );
+
+                }).toList();
+
+        return new ReportResponse(clientId, accountReports);
     }
 }
