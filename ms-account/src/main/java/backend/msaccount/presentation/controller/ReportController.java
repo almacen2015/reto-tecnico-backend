a41db59ed59058ac89ef7950cd0f6f1606019e25
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2026-04-26 21:01:22 -0500

    se agrego Report controller

---
+++ ms-account/src/main/java/backend/msaccount/presentation/controller/ReportController.java
@@ -0,0 +1,30 @@
+package backend.msaccount.presentation.controller;
+
+import backend.msaccount.application.usecase.GenerateReportUseCase;
+import backend.msaccount.presentation.dto.ReportResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/v1/reports")
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final GenerateReportUseCase useCase;
+
+    @GetMapping("/{clientId}")
+    public Mono<ResponseEntity<ReportResponse>> generateReport(
+            @PathVariable("clientId") Long clientId,
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalDate endDate
+    ) {
+        return Mono.fromCallable(() -> useCase.execute(clientId, startDate, endDate))
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(ResponseEntity::ok);
+    }
+}
